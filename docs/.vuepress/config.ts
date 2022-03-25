86c25bb2afefdd1c69202547a15ddcffdd531845
deguchi <deguchik@gmail.com>
2022-03-25 15:34:10 +0900

    first commit

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,27 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+import { path } from '@vuepress/utils'
+
+export default defineUserConfig<DefaultThemeOptions>({
+  // site config
+  lang: 'ja-JP',
+  title: 'VuePressのサンプル',
+  description: 'Just playing around',
+  base: '/',
+  // theme and its config
+//   theme: '@vuepress/theme-default',
+//   themeConfig: {
+//     logo: 'https://vuejs.org/images/logo.png',
+//   },
+    themeConfig: {
+        logo: '/images/logo.png',
+        sidebar : [
+            { text: 'はじめに', link: '/' },
+            { text: 'テスト', link: '/test' },
+        ]
+    },
+    theme: path.resolve(__dirname, './theme'),
+    plugins: [
+        ['@vuepress/plugin-search']
+    ],
+})
---
+++ docs/.vuepress/theme/layouts/layout.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Layout from '@vuepress/theme-default/lib/client/layouts/Layout.vue'
+
+export default defineComponent({
+  components: { Layout },
+})
+</script>
+
+<template>
+  <Layout>
+    <template #sidebar-top>
+      <div class="sidebar-header">サイドバー</div>
+    </template>
+  </Layout>
+</template>
+
+<style>
+.sidebar-header {
+  padding: 1.5rem 1.5rem 0;
+  font-size: 1.1em;
+  font-weight: bold;
+}
+</style>
