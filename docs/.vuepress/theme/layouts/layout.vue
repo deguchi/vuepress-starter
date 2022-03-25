518340ce6381be6281c152b0778612b5c001c16b
deguchi <deguchik@gmail.com>
2022-03-25 15:56:04 +0900

    update

--- docs/.vuepress/theme/layouts/layout.vue
+++ docs/.vuepress/theme/layouts/layout.vue
@@ -1,24 +1,31 @@
-<script lang="ts">
-import { defineComponent } from 'vue'
-import Layout from '@vuepress/theme-default/lib/client/layouts/Layout.vue'
-
-export default defineComponent({
-  components: { Layout },
-})
+<script setup>
+import ParentLayout from '@vuepress/theme-default/lib/client/layouts/Layout.vue'
 </script>
 
 <template>
-  <Layout>
+  <ParentLayout>
+    <template #page-top>
+      <div class="my-header">This is my custom page footer</div>
+    </template>
     <template #sidebar-top>
       <div class="sidebar-header">サイドバー</div>
     </template>
-  </Layout>
+    <template #page-bottom>
+      <div class="my-footer">This is my custom page footer</div>
+    </template>
+  </ParentLayout>
 </template>
 
-<style>
+<style lang="css">
+.my-header {
+  text-align: center;
+}
 .sidebar-header {
   padding: 1.5rem 1.5rem 0;
   font-size: 1.1em;
   font-weight: bold;
+}
+.my-footer {
+  text-align: center;
 }
 </style>
